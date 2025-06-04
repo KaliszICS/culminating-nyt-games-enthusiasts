e6342931ba0287860dcc2c6afc608674d841fbb3
Elliot Chan <elliot.chan@student.tdsb.on.ca>
2025-06-04 14:44:00 +0000

    a

---
+++ src/main/java/GraphicsHandler.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GraphicsHandler {
+    
+
+    public void initiate() {
+        JFrame frame = new JFrame("NYT Games");
+        frame.setSize(1920, 1080);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+        
+}
